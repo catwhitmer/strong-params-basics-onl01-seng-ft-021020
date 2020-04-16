41dfb13ce185d904cd836fa08bf14863147a4340
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-16 23:05:35 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,11 +17,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
  
-def update
-  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to post_path(@post)
-end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
  
 private
  
